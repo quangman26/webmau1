c475785e9ae92cd48b36ea0aceaabb5b24ea807a
PHAN QUANG MAN <manquang087@gmail.com>
2026-05-22 16:52:54 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,1 @@
-/* =========================================
-   HERO SLIDER
-========================================= */
 
-const heroSlides = document.querySelectorAll(".hero-slide");
-
-let heroIndex = 0;
-
-if (heroSlides.length > 0) {
-  setInterval(() => {
-    heroSlides[heroIndex].classList.remove("active");
-
-    heroIndex = (heroIndex + 1) % heroSlides.length;
-
-    heroSlides[heroIndex].classList.add("active");
-  }, 5000);
-}
